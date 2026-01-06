953a4d24f6bb9bb659be9230cc1d065aacee43c4
SJ <qkdtjdwls93@naver.com>
2026-01-06 16:50:07 +0900

    스프링시작

---
+++ src/main/java/com/simpol/polisight/PolisightApplication.java
@@ -0,0 +1,13 @@
+package com.simpol.polisight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolisightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolisightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpol/polisight/PolisightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpol.polisight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolisightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
