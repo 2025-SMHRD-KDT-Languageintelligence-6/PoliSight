8cd3654458c82bf7b95d4cd35761d83ce2bcaf2a
bsb0107 <bsb0107@naver.com>
2026-01-31 16:51:23 +0900

    서버 도메인 수정

--- src/main/java/com/simpol/polisight/config/SecurityConfig.java
+++ src/main/java/com/simpol/polisight/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -40,6 +41,11 @@ public class SecurityConfig {
                 .passwordEncoder(passwordEncoder);
 
         return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 
     @Bean
